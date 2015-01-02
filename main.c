dac85168cb38b3c1422153a3d5c030d0e185a78e
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2015-01-02 03:58:51 -0800

    added some comments; added a to do list; use of '->' instead of '(* )' when accessing structure members through pointers

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/* To do list:
+ *  - MAYBE make seperate files for each variable type; easier to manage
+ *  - MAYBE make a global table, in which all variables reside
+ *  - add booleans, lists, strings, and functions
+ *  - finish showTable()
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,7 +18,7 @@ typedef struct tableseg tableseg;
 
 
 table *newTable (void);
-var *newVar (unsigned short type, void *value);
+var *newVar (unsigned char type, void *value);
 void addToTable (table *tablep, var *varp);
 void rmvFromTable (table *tablep, unsigned int i);
 char *varValStr (var *varp);
@@ -32,7 +40,7 @@ struct tableseg {
 };
 
 struct var {
-  unsigned short type;
+  unsigned char type;
   void *value;
 };
 
@@ -53,12 +61,16 @@ int main (void)
   valStrFuncs[TYPE_NUMBER] = numToStr;
   valStrFuncs[TYPE_STRING] = strToStr;
   valStrFuncs[TYPE_TABLE] = tableToStr;
+  
+  /* Test */
   table *table1 = newTable();
   showTable(table1);
-  double x = 7.2422;
+  double x = 9000.1;
   var *var1 = newVar(TYPE_NUMBER, (void*)&x);
   addToTable(table1, var1);
   showTable(table1);
+  /* EOF Test */
+  
   return 0;
 }
 
@@ -66,15 +78,15 @@ int main (void)
 table *newTable (void)
 {
   table *tablep = (table*)malloc(sizeof(table));
-  (*tablep).size = 0;
+  tablep->size = 0;
   return tablep;
 }
 
-var *newVar (unsigned short type, void *value)
+var *newVar (unsigned char type, void *value)
 {
   var *varp = (var*)malloc(sizeof(var));
-  (*varp).type = type;
-  (*varp).value = value;
+  varp->type = type;
+  varp->value = value;
   return varp;
 }
 
@@ -82,45 +94,46 @@ void addToTable (table *tablep, var *varp)
 {
   // Add to beginning of stack.
   tableseg *newseg = (tableseg*)malloc(sizeof(tableseg));
-  (*newseg).varp = varp;
-  (*newseg).next = (*tablep).first;
-  (*tablep).first = newseg;
-  (*tablep).size += 1;
+  newseg->varp = varp;
+  newseg->next = tablep->first;
+  tablep->first = newseg;
+  tablep->size += 1;
 }
 
 void rmvFromTable (table *tablep, unsigned int i)
 {
-  if (i > (*tablep).size) {
+  if (i > tablep->size) {
     return;
   }
   tableseg *before;
   tableseg *after;
-  before = (*tablep).first;
+  before = tablep->first;
   int cnt;
   for (cnt=1; cnt<i-1; cnt++)
-    before = (*before).next;
-  after = (*before).next;
+    before = before->next;
+  after = before->next;
   if (after) {
-    after = (*after).next;
-    free((void*)(*before).next);
+    after = after->next;
+    free((void*)before->next);
   }
-  (*before).next = after;
-  (*tablep).size -= 1;
+  before->next = after;
+  tablep->size -= 1;
 }
 
 char *varValStr (var *varp)
 {
-  valStrFuncs[(*varp).type]((*varp).value);
+  valStrFuncs[varp->type](varp->value);
 }
 
 char *listToStr (void *value)
 {
+  /* Change later */
   return "DOIT";
 }
 
 char *boolToStr (void *value)
 {
-  if ((short*)value == 0) {
+  if ((char*)value == 0) {
     return "FALSE";
   } else {
     return "TRUE";
@@ -129,6 +142,7 @@ char *boolToStr (void *value)
 
 char *tableToStr (void *value)
 {
+  /* Change later */
   return "DOIT";
 }
 
@@ -146,20 +160,20 @@ char *strToStr (void *value)
 
 void showTable (table *tablep)
 {
-  if ((*tablep).first == NULL) {
+  if (tablep->first == NULL) {
     puts("nothing in table");
     return;
   }
   tableseg *cur;
   var *varp;
-  cur = (*tablep).first;
+  cur = tablep->first;
   while (cur != NULL) {
-    varp = (*cur).varp;
+    varp = cur->varp;
     char str[60];
-    strcpy(str, typenames[(*varp).type]);
+    strcpy(str, typenames[varp->type]);
     strcat(str, "\t");
     strcat(str, varValStr(varp));
     puts(str);
-    cur = (*cur).next;
+    cur = cur->next;
   }
 }
