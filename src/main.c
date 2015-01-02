723b6e1d394f840359fa8b7c35f8b9de78bb17da
Robert Cochran <robert-git@cochranmail.com>
2015-01-02 15:54:07 -0800

    Fix all warnings from new GCC flags

--- src/main.c
+++ src/main.c
@@ -11,6 +11,8 @@
 #include <string.h>
 #include "type.h"
 
+#define UNUSED(x) (void)(x)
+
 typedef struct table {
   unsigned int size;
   struct tableseg *first;
@@ -39,7 +41,8 @@ char *numToStr (void *value);
 char *strToStr (void *value);
 void showTable (struct table_t *tablep);
 
-char *typenames[5];
+const char *typenames[5];
+
 char *(*valStrFuncs[5]) (void *value);
 
 int main (void)
@@ -116,28 +119,30 @@ void rmvFromTable (struct table_t *tablep, unsigned int i)
 
 char *varValStr (struct var_t *varp)
 {
-  valStrFuncs[varp->type](varp->value);
+  return valStrFuncs[varp->type](varp->value);
 }
 
 char *listToStr (void *value)
 {
   /* Change later */
-  return "DOIT";
+  UNUSED(value);
+  return (char *) "DOIT";
 }
 
 char *boolToStr (void *value)
 {
   if ((char*)value == 0) {
-    return "FALSE";
+	  return (char *) "FALSE";
   } else {
-    return "TRUE";
+	  return (char *) "TRUE";
   }
 }
 
 char *tableToStr (void *value)
 {
   /* Change later */
-  return "DOIT";
+  UNUSED(value);
+  return (char *) "DOIT";
 }
 
 char *numToStr (void *value)
