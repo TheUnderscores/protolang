cfbdab3377ffc491f923e6bde27f7eff7134efec
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2015-01-02 03:11:55 -0800

    Moved preprocessor stuff from main.c to type.h and inclduded it in main.c

--- main.c
+++ main.c
@@ -1,28 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#ifndef TYPE_ARRAY
-#define TYPE_ARRAY   0
-#endif
-#ifndef TYPE_BOOLEAN
-#define TYPE_BOOLEAN 1
-#endif
-#ifndef TYPE_LIST
-#define TYPE_LIST    2
-#endif
-#ifndef TYPE_NUMBER
-#define TYPE_NUMBER  3
-#endif
-#ifndef TYPE_STRING
-#define TYPE_STRING  4
-#endif
-#ifndef TRUE
-#define TRUE  1
-#endif
-#ifndef FALSE
-#define FALSE 0
-#endif
+#include "type.h"
 
 
 typedef struct var var;
---
+++ type.h
@@ -0,0 +1,21 @@
+#ifndef TYPE_ARRAY
+#define TYPE_ARRAY   0
+#endif
+#ifndef TYPE_BOOLEAN
+#define TYPE_BOOLEAN 1
+#endif
+#ifndef TYPE_LIST
+#define TYPE_LIST    2
+#endif
+#ifndef TYPE_NUMBER
+#define TYPE_NUMBER  3
+#endif
+#ifndef TYPE_STRING
+#define TYPE_STRING  4
+#endif
+#ifndef TRUE
+#define TRUE  1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
